82ee0b3d304d192c36c64c70632adf312d799572
IvaylaCh <ivaylachomakovska@gmail.com>
2025-07-28 15:28:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace gps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(60, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Първи град";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(231, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Втори град";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(51, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 68);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "най-кратно разстояние";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(256, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(56, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(225, 110);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(129, 27);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 22);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Джобен GPS";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.ForeColor = System.Drawing.Color.MintCream;
+            this.button1.Location = new System.Drawing.Point(423, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 100);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Изчисли";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(45, 226);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(614, 27);
+            this.textBox2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(723, 287);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Ивайла, Теодор, Константин-Кирил,  Явор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gps
+{
+    public partial class Form1 : Form
+    {
+        private const string FilePath = "path.txt"; // Path to the text file containing city distances
+
+        private Dictionary<string, Dictionary<string, int>> graph;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCities();
+        }
+
+        private void InitializeCities()
+        {
+            // Add cities to ComboBoxes
+            string[] cities = { "София", "Пловдив", "Варна", "Бургас", "Русе", "Стара Загора", "Плевен", "Сливен", "Добрич", "Шумен", "Перник", "Хасково", "Ямбол", "Пазарджик", "Благоевград" };
+
+            comboBox1.Items.AddRange(cities);
+            comboBox2.Items.AddRange(cities);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadCityDistances();
+            MessageBox.Show("Първо избери град");
+        }
+
+        private void LoadCityDistances()
+        {
+            // Read city distances from the text file and populate the graph
+            graph = new Dictionary<string, Dictionary<string, int>>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(FilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        string city1 = parts[0];
+                        string city2 = parts[1];
+                        int distance = int.Parse(parts[2]);
+
+                        if (!graph.ContainsKey(city1))
+                            graph[city1] = new Dictionary<string, int>();
+
+                        if (!graph.ContainsKey(city2))
+                            graph[city2] = new Dictionary<string, int>();
+
+                        graph[city1][city2] = distance;
+                        graph[city2][city1] = distance;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Грешка: " + ex.Message, "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+      
+
+        private Dictionary<string, int> Dijkstra(Dictionary<string, Dictionary<string, int>> graph, string start)
+        {
+            Dictionary<string, int> distances = new Dictionary<string, int>();
+            HashSet<string> visited = new HashSet<string>();
+
+            foreach (string vertex in graph.Keys)
+            {
+                distances[vertex] = int.MaxValue;
+            }
+
+            distances[start] = 0;
+
+            while (visited.Count < graph.Count)
+            {
+                string currentVertex = GetMinDistanceVertex(distances, visited);
+                visited.Add(currentVertex);
+
+                foreach (var neighbor in graph[currentVertex])
+                {
+                    int alternativeRoute = distances[currentVertex] + neighbor.Value;
+                    if (alternativeRoute < distances[neighbor.Key])
+                    {
+                        distances[neighbor.Key] = alternativeRoute;
+                    }
+                }
+            }
+
+            return distances;
+        }
+
+        private string GetMinDistanceVertex(Dictionary<string, int> distances, HashSet<string> visited)
+        {
+            return distances.Where(x => !visited.Contains(x.Key)).OrderBy(x => x.Value).First().Key;
+        }
+
+        private List<string> GetShortestPath(Dictionary<string, int> distances, string start, string end)
+        {
+            List<string> path = new List<string>();
+
+            if (distances[end] == int.MaxValue)
+                return null; // No path found
+
+            string currentVertex = end;
+            while (!currentVertex.Equals(start))
+            {
+                path.Insert(0, currentVertex);
+                foreach (var neighbor in graph[currentVertex])
+                {
+                    if (distances[currentVertex] == distances[neighbor.Key] + neighbor.Value)
+                    {
+                        currentVertex = neighbor.Key;
+                        break;
+                    }
+                }
+            }
+
+            path.Insert(0, start);
+            return path;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string startCity = comboBox1.SelectedItem.ToString();
+            string endCity = comboBox2.SelectedItem.ToString();
+
+            if (graph.ContainsKey(startCity) && graph.ContainsKey(endCity))
+            {
+                Dictionary<string, int> distances = Dijkstra(graph, startCity);
+                List<string> path = GetShortestPath(distances, startCity, endCity);
+
+                if (path != null)
+                {
+                    int totalDistance = distances[endCity];
+                    string pathString = string.Join(" - ", path);
+
+                    textBox1.Text = $"{totalDistance+ " км."}";
+                    textBox2.Text=$" {pathString}";
+                }
+                else
+                {
+                    MessageBox.Show("Няма открита пътека.", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Няма избран град.", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+    /*public partial class Form1 : Form
+    {
+        string[] cities;
+        private int[,] graph;
+        string fileInfo = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void WriteCities(string fileDirectory)
+
+        {
+            using (StreamReader reader = new StreamReader(fileDirectory, Encoding.UTF8))
+            {
+                StringBuilder sb = new StringBuilder();
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    sb.AppendLine(line);
+                }
+                fileInfo = sb.ToString().TrimEnd();
+            }
+        }
+        public void FillUpComboBoxes()
+        {
+            for (int i = 0; i < cities.Length && cities[i] != null; i++)
+            {
+                comboBox1.Items.Add(cities[i]);
+                comboBox2.Items.Add(cities[i]);
+            }
+        }
+
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            WriteCities("path.txt");
+            ParseCitiesAndDistances();
+            FillUpComboBoxes();
+        }
+        private void ParseCitiesAndDistances()
+        {
+            string[] fileInfoSeparatedByNewLine = fileInfo.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            cities = new string[fileInfoSeparatedByNewLine.Length * 2];
+            graph = new int[cities.Length, cities.Length];
+
+            int index = 0;
+
+            for (int i = 0; i < fileInfoSeparatedByNewLine.Length; i++)
+            {
+                string[] citiesAndDistance = fileInfoSeparatedByNewLine[i].Split(' ');
+                string city1 = citiesAndDistance[0];
+                string city2 = citiesAndDistance[1];
+
+                if (!string.IsNullOrEmpty(city1) && !Array.Exists(cities, element => element == city1))
+                    cities[index++] = city1;
+
+                if (!string.IsNullOrEmpty(city2) && !Array.Exists(cities, element => element == city2))
+                    cities[index++] = city2;
+
+                int city1Index = Array.IndexOf(cities, city1);
+                int city2Index = Array.IndexOf(cities, city2);
+                int distance = int.Parse(citiesAndDistance[2]);
+
+                graph[city1Index, city2Index] = distance;
+                graph[city2Index, city1Index] = distance;
+            }
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            for (int i = 0; i < cities.Length && cities[i] != null; i++)
+            {
+                comboBox2.Items.Add(cities[i]);
+            }
+            comboBox2.Items.Remove(comboBox1.Text);
+            if (comboBox2.Text != "") 
+            FindTheShortestDistance(comboBox1.Text, comboBox2.Text);
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            comboBox1.Items.Clear();
+            for (int i = 0; i < cities.Length && cities[i] != null; i++)
+            {
+                comboBox1.Items.Add(cities[i]);
+            }
+            comboBox1.Items.Remove(comboBox2.Text);
+            if (comboBox1.Text != "") 
+            FindTheShortestDistance(comboBox1.Text, comboBox2.Text);
+        }
+
+        private void FindTheShortestDistance(string firstCity, string secondCity)
+        {
+            bool[] used = new bool[cities.Length];
+            int[] distances = new int[cities.Length];
+            int[] parents = new int[cities.Length];
+            int[,] graph = new int[cities.Length, cities.Length];
+            int startIndex = Array.IndexOf(cities,firstCity);
+            int endIndex = Array.IndexOf(cities, secondCity);
+            List<string> fileInfoSeparatedByNewLine = fileInfo.Split(new string[] { Environment.NewLine },
+    StringSplitOptions.None).ToList();
+            //algorithm start
+            for (int i = 0; i < fileInfoSeparatedByNewLine.Count; i++)
+            {
+                string[] citiesAndDistance = fileInfoSeparatedByNewLine[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                int city1 = int.Parse(citiesAndDistance[0]) - 1;
+                int city2 = int.Parse(citiesAndDistance[1]) - 1;
+                int distance = int.Parse(citiesAndDistance[2]);
+                graph[city1, city2] = distance;
+                graph[city2, city1] = distance;
+            }
+            for (int i = 0; i < distances.Length; i++)
+            {
+                distances[i] = int.MaxValue;
+                parents[i] = -1;
+            }
+            distances[startIndex] = 0;
+            int currentIndex = startIndex;
+            while (!used[currentIndex])
+            {
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[currentIndex, i] != 0)
+                    {
+                        int dist = graph[currentIndex, i] + distances[currentIndex];
+                        if (dist < distances[i])
+                        {
+                            distances[i] = dist;
+                            parents[i] = currentIndex;
+                        }
+                    }
+                }
+                used[currentIndex] = true;
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (distances[i] < int.MaxValue && !used[i])
+                    {
+                        currentIndex = i;
+                        break;
+                    }
+                }
+            }
+            List<int> shortestPath = new List<int>();
+            //algorithm end
+            int current = endIndex;
+            while (current != startIndex)
+            {
+                shortestPath.Add(current);
+                current = parents[current];
+            }
+           // shortestPath.Add(Array.IndexOf());
+            shortestPath.Reverse();
+            for (int i = 0; i < shortestPath.Count; i++)
+            {
+                listBox1.Items.Add(cities[shortestPath[i]]);
+            }
+            textBox1.Text = distances[endIndex].ToString();
+
+        }
+    }*/
+
+    
+
+
+    
+
+
+
+
+    
+
+
+
